e72a87a694ba61b8f68d967fed5e49d3a89898d1
bot_jang <katatj7@gmail.com>
2021-04-14 23:07:22 +0900

    5-5) 내가 푼것

--- Part 1,2/ch5)practice.R
+++ Part 1,2/ch5)practice.R
@@ -124,4 +124,25 @@ library(dplyr)
 mpg_raw <- mpg_new  
 mpg_raw
 
-###
+###part5-5###
+#• Q1. mpg 데이터의 class 는 "suv", "compact" 등 자동차를 특징에 따라 일곱 종류로 분류한 변수입니다. 어떤 차종의 연비가 높은지 비교해보려고 합니다. class 별 cty 평균을 구해보세요.
+mpg_new2 <- as.data.frame(ggplot2::mpg)
+mpg_summary <- mpg_new2 %>% 
+  group_by(class, cty) %>% 
+  summarise(mean_cty = mean(cty))
+View(mpg_new2)
+
+#• Q2. 앞 문제의 출력 결과는 class 값 알파벳 순으로 정렬되어 있습니다. 어떤 차종의 도시 연비가 높은지쉽게 알아볼 수 있도록 cty 평균이 높은 순으로 정렬해 출력하세요.
+mpg_summary %>% 
+  arrange(desc(mean_cty))
+
+#• Q3. 어떤 회사 자동차의 hwy(고속도로 연비)가 가장 높은지 알아보려고 합니다. hwy 평균이 가장 높은 회사 세 곳을 출력하세요.
+mpg_new2 %>%   
+  group_by(manufacturer, hwy) %>% 
+  summarise(mean_hwy = mean(hwy)%>%
+  head(3)
+
+#• Q4. 어떤 회사에서 "compact"(경차) 차종을 가장 많이 생산하는지 알아보려고 합니다. 각 회사별 "compact" 차종 수를 내림차순으로 정렬해 출력하세요.
+mpg_new2 %>% 
+  group_by(manufacturer, class) %>% 
+  head(3)
