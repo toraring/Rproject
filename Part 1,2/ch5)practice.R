aa1f3b7c98718729e12947934ce05fa741732da7
bot_jang <katatj7@gmail.com>
2021-04-14 23:25:08 +0900

    05. 집단 별로 데이터 요약하기+답

--- Part 1,2/ch5)practice.R
+++ Part 1,2/ch5)practice.R
@@ -132,17 +132,45 @@ mpg_summary <- mpg_new2 %>%
   summarise(mean_cty = mean(cty))
 View(mpg_new2)
 
+#답
+mpg <- as.data.frame(ggplot2::mpg) # mpg 데이터 불러오기
+mpg %>%
+  group_by(class) %>% # class 별 분리
+  summarise(mean_cty = mean(cty)) # cty 평균 구하기
+##
+
 #• Q2. 앞 문제의 출력 결과는 class 값 알파벳 순으로 정렬되어 있습니다. 어떤 차종의 도시 연비가 높은지쉽게 알아볼 수 있도록 cty 평균이 높은 순으로 정렬해 출력하세요.
 mpg_summary %>% 
-  arrange(desc(mean_cty))
+  arrange(desc(mean_cty))#파생변수 있어야함
+
+#답
+mpg %>%
+  group_by(class) %>% # class 별 분리
+  summarise(mean_cty = mean(cty)) %>% # cty 평균 구하기
+  arrange(desc(mean_cty)) # 내림차순 정렬하기
+##
 
 #• Q3. 어떤 회사 자동차의 hwy(고속도로 연비)가 가장 높은지 알아보려고 합니다. hwy 평균이 가장 높은 회사 세 곳을 출력하세요.
 mpg_new2 %>%   
   group_by(manufacturer, hwy) %>% 
   summarise(mean_hwy = mean(hwy)%>%
   head(3)
-
+  
+#답
+mpg %>%
+  group_by(manufacturer) %>% # manufacturer 별 분리
+  summarise(mean_hwy = mean(hwy)) %>% # hwy 평균 구하기
+  arrange(desc(mean_hwy)) %>% # 내림차순 정렬하기
+  head(3) # 상위 3 행 출력
+  
 #• Q4. 어떤 회사에서 "compact"(경차) 차종을 가장 많이 생산하는지 알아보려고 합니다. 각 회사별 "compact" 차종 수를 내림차순으로 정렬해 출력하세요.
 mpg_new2 %>% 
   group_by(manufacturer, class) %>% 
   head(3)
+
+#답
+mpg %>%
+  filter(class == "compact") %>% # compact 추출
+  group_by(manufacturer) %>% # manufacturer 별 분리
+  summarise(count = n()) %>% # 빈도 구하기
+  arrange(desc(count)) # 내림차순 정렬
