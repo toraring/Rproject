3fbeeb35731173189b2b9985a67bc9f63542d671
bot_jang <katatj7@gmail.com>
2021-04-16 01:27:54 +0900

    01. 변수 간 관계를 표현하는 산점도 + 답 및 정리

--- Part 3/ch6)Practice.R
+++ Part 3/ch6)Practice.R
@@ -7,7 +7,16 @@ mpg <- as.data.frame(ggplot2::mpg)
 ggplot(data = mpg, aes(x = cty, y = hwy)) + geom_point( )
 
 #답
+#위와 동일
 
 #• Q2. 미국 지역별 인구통계 정보를 담은 ggplot2 패키지의 midwest 데이터를 이용해서 전체 인구와 아시아인 인구 간에 어떤 관계가 있는지 알아보려고 합니다. x 축은 poptotal(전체 인구), y 축은 popasian(아시아인 인구)으로 된 산점도를 만들어 보세요. 전체 인구는 50 만 명 이하, 아시아인 인구는 1 만 명 이하인 지역만 산점도에 표시되게 설정하세요.
 ggplot(data = midwest, aes(x = poptotal, y = popasian)) + geom_point() + xlim(0, 500000)+ ylim(0, 10000)
 
+#답
+#위와동일
+
+#ggplot은 하이픈픈 %>% 대신 + 로 연결
+#xlim, ylim은 범위를 지정해주는것.. 범위지정안하면 x, y축 범위가 너무 커진다
+
+###
+##
