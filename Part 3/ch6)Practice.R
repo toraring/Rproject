2c4f2542e196b3897e8e5cd08e99b69d9bcb6948
bot_jang <katatj7@gmail.com>
2021-04-17 01:51:17 +0900

    02. 집단 간 차이를 표현하는 막대 그래프 + 실습 및 답

--- Part 3/ch6)Practice.R
+++ Part 3/ch6)Practice.R
@@ -15,8 +15,43 @@ ggplot(data = midwest, aes(x = poptotal, y = popasian)) + geom_point() + xlim(0,
 #답
 #위와동일
 
-#ggplot은 하이픈픈 %>% 대신 + 로 연결
+#ggplot은 하이픈 %>% 대신 + 로 연결
 #xlim, ylim은 범위를 지정해주는것.. 범위지정안하면 x, y축 범위가 너무 커진다
 
 ###
-##
+##part 6-2
+#• Q1. 어떤 회사에서 생산한 "suv" 차종의 도시 연비가 높은지 알아보려고 합니다. "suv" 차종을 대상으로 평균 cty(도시 연비)가 가장 높은 회사 다섯 곳을 막대 그래프로 표현해 보세요. 막대는 연비 가 높은 순으로 정렬하세요.
+library(ggplot2)
+library(dplyr)
+mpg <- as.data.frame(ggplot2::mpg)
+
+df_mpg <- mpg %>% 
+  filter(class = "suv")
+  group_by(cty) %>% 
+  summarise(mean_cty = mean(cty))
+View(mpg)
+
+ggplot(data = mpg, aes((x = reorder("suv", -mean_cty), y = mean_cty) + geom_col
+
+#답
+# 평균 표 생성
+df <- mpg %>%
+  filter(class == "suv") %>%
+  group_by(manufacturer) %>%
+  summarise(mean_cty = mean(cty)) %>%
+  arrange(desc(mean_cty)) %>%
+  head(5)
+# 그래프 생성
+ggplot(data = df, aes(x = reorder(manufacturer, -mean_cty), y = mean_cty)) + geom_col()
+
+                      
+#• Q2. 자동차 중에서 어떤 class (자동차 종류)가 가장 많은지 알아보려고 합니다. 자동차 종류별 빈도를 표현한 막대 그래프를 만들어 보세요.
+df_mpg <- mpg %>% 
+  group_by(class) %>% 
+  summarise(mean_hty = mean(cty))
+View(mpg)
+
+ggplot(data = mpg, aes(x = cty)) + geom_bar
+
+#답
+ggplot(data = mpg, aes(x = class)) + geom_bar()
