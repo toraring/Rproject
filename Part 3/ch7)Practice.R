07a93108df42bc3402258e0a6b24d41af19936df
bot_jang <katatj7@gmail.com>
2021-04-19 04:25:07 +0900

    7-2. 이상치 확인 + 실습 및 답

--- Part 3/ch7)Practice.R
+++ Part 3/ch7)Practice.R
@@ -20,4 +20,44 @@ mean(mpg_nomiss$hwy)
 mpg %>%
   filter(!is.na(hwy)) %>% # 결측치 제외
   group_by(drv) %>% # drv 별 분리
-  summarise(mean_hwy = mean(hwy)) # hwy 평균 구하기
+  summarise(mean_hwy = mean(hwy)) # hwy 평균 구하기 
+
+###part 7-2. 이상치
+##우선 mpg 데이터를 불러와서 일부러 이상치를 만들겠습니다. drv(구동방식) 변수의 값은 4(사륜구동), f(전륜구동), r(후륜구동) 세 종류로 되어있습니다. 몇 개의 행에 존재할 수 없는 값 k를 할당하겠습니다. cty(도시 연비) 변수도 몇 개의 행에 극단적으로 크거나 작은 값을 할당하겠습니다.
+
+mpg <- as.data.frame(ggplot2::mpg) # mpg 데이터 불러오기
+mpg[c(10, 14, 58, 93), "drv"] <- "k" # drv 이상치 할당
+mpg[c(29, 43, 129, 203), "cty"] <- c(3, 4, 39, 42) # cty 이상치 할당
+
+#Q1. drv 에 이상치가 있는지 확인하세요. 이상치를 결측 처리한 다음 이상치가 사라졌는지 확인하세요. 결측 처리 할 때는 %in% 기호를 활용하세요.
+table(mpg$drv) <- ifelse(mpg$drv 
+
+#답
+# 이상치 확인
+table(mpg$drv)
+# drv 가 4, f, r 이면 기존 값 유지, 그 외 NA 할당
+mpg$drv <- ifelse(mpg$drv %in% c("4", "f", "r"), mpg$drv, NA)
+# 이상치 확인
+table(mpg$drv)
+
+#Q2. 상자 그림을 이용해서 cty 에 이상치가 있는지 확인하세요. 상자 그림의 통계치를 이용해 정상 범위를 벗어난 값을 결측 처리한 후 다시 상자 그림을 만들어 이상치가 사라졌는지 확인하세요.
+table(is.na(mpg$cty)
+#%in%은 매칭확인
+boxplot(mpg$cty)$stats
+mpg$cty <- ifelse(mpg$cty %in% c(3, 4, 39, 42), NA, mpg$cty)
+
+#답
+# 상자 그림 생성 및 통계치 산출
+boxplot(mpg$cty)$stats
+# 9~26 벗어나면 NA 할당
+mpg$cty <- ifelse(mpg$cty < 9 | mpg$cty > 26, NA, mpg$cty)
+.# 상자 그림 생성
+boxplot(mpg$cty)
+
+#Q3. 두 변수의 이상치를 결측처리 했으니 이제 분석할 차례입니다. 이상치를 제외한 다음 drv 별로 cty 평균이 어떻게 다른지 알아보세요. 하나의 dplyr 구문으로 만들어야 합니다.
+mpg %>%
+  filter(!is.na(drv) & !is.na(cty)) %>%
+  group_by(drv) %>%
+  summarise(mean_cty = mean(cty))
+
+#답은 위와 동일
