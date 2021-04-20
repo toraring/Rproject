de92b88b0ef8c9053c61a6cbd7afd54d27d5833e
bot_jang <katatj7@gmail.com>
2021-04-20 22:34:56 +0900

    연습

---
+++ Part 4/ch1.R
@@ -0,0 +1,6 @@
+#1.두집단 차이검정
+#1.1.각 집단 표본 수 = 3
+#1.1.1.파일 불러오기
+setwd("C:\RProject\Rproject\Part 4\Data\￿￿/R￿￿￿￿￿￿￿￿￿/")
+rawN3 <- read.csv(file="C:\RProject\Rproject\Part 4\Data\/htest01.csv, header = TRUE)
+head(rawN3)
