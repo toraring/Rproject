8e8b845f594c14bcd433f708a54ae79b37cec499
bot_jang <katatj7@gmail.com>
2021-04-22 00:44:17 +0900

    이론 추가

--- Part 1,2/이론.R
+++ Part 1,2/이론.R
@@ -30,3 +30,28 @@ f <- seq(1, 5)
 g <- seq(1, 10, by=2) # 1~10까지 2씩 증가
 c2 <- "Hello world!"
 e2 <- c("Hello!", "World", "is", "good!")
+paste(e2, collapse = " ") # 빈칸 구분자로 문자 붙이기
+
+#패키지 이용
+library(ggplot2) 
+qplot(b)
+qplot(data = mpg, y = hwy, x = drv, geom = "point")
+
+#데이프레임 만들기
+history <- c(90, 80, 60, 70) # 역사점수 생성
+math <- c(50, 60, 100, 20) # 수학점수 생성
+# 변수 합해서 데이터프레임 만들기
+df_midterm <- data.frame(history, math) 
+df_midterm
+
+#외부 데이터 불러오기
+# readxl 패키지 설치 
+install.packages("readxl")
+# readxl 패키지 로드
+library(readxl)
+# 엑셀 파일 불러오기 
+df_finalexam <- read_excel("finalexam.xlsx", sheet = 1, col_names = T)
+# 첫번째 행 변수명으로 로드
+read_excel("finalexam.xlsx", sheet = 1, col_names = T)
+#csv로 저장
+write.csv(df_finalexam, file = "output_newdata.csv")
