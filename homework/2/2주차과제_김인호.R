09f87bda833da6d4bd7a16a24e2927db399bbb6c
bot_jang <katatj7@gmail.com>
2021-04-28 00:47:30 +0900

    정답체크

--- homework/2/2주차과제_김인호.R
+++ homework/2/2주차과제_김인호.R
@@ -45,6 +45,13 @@ dat_high <- dat %>% filter(floors != 1)
 mean(dat_low$price)
 mean(dat_high$price)
 
+#정답
+kc_house %>% group_by(floor_level) %>%
+  summarise(mean_price = mean(price))
+#> floor_level 그룹에 따른 평균 가격을 산출하기 위해 group_by와 summarise함수를 사용합니다.
+#> group_by를 쓰지 않아도 코드가 비교적 짧게 완성되지만, 범주가 매우 많은 경우 group_by를 쓰지 않으면 범주별로 평균 집값을 출력하는 코드를 작성하는 것이 매우 비효율적입니다.
+#따라서 어떤 범주 별로 요약통계량을 구할 때는 group_by와 summarise를 자주 사용해주시기 바랍니다:)
+
 # (10점) 5. grade가 4인 집과 13인 집의 평균 집 값을 비교하고 싶습니다. 
 # grade가 4인 집은 group1, 13인 집은 group2라는 새로운 이름의 데이터 프레임을 생성하세요. 이 때, 변수는 grade와 price만 추출하세요.
 group1 <- dat[dat$grade==4,1:12] %>% select(grade, price)
@@ -57,11 +64,23 @@ group2 <- dat[dat$grade==13,1:12] %>% select(grade, price)
 
 t.test(group1[,2], group2[,2], var.equal = TRUE, alternative="less")
 
+#정답
+t.test(group1[,2], group2[,2], alternative= 'two.sided',var.equal = TRUE)
+#> 정규성 가정과 등분산 가정도 만족한다고 문제에서 주어졌기 때문에 var.equal=TRUE
+#> 문제에서 대립가설을 '다르다' 라고 주었기 떄문에 alternative = 'two.sided'가 아닌 다른 것으로 작성하시면 오답처리 됩니다.
+#> var.equal 의 디폴트 값은 FALSE이므로 TRUE로 지정하지 않으시면 오답처리 됩니다.
+
+
 # (5점) 6-(2). 위 결과를 해석하세요.
 # 해석 :  
 p-value는 0.05보다 작다. 
 그래서 대립가설을 채택하므로 
 grade가 4인 집과 13인 집의 가격은 다르다.
+#정답
+p-value가 9.395e-13으로 유의수준 0.05보다 작으므로 귀무가설을 기각한다.
+> 9.395e-13은 지수 표현식으로, e-13은 10^(-13) 과 동일한 뜻입니다.
+> Tip. options(scipen = 999) 를 실행하면 지수표현식 대신 기존의 표기대로 표시됩니다. 지수표기법으로 복귀는 options(scipen=0)
+
 
 # r에서는 문자열의 위치에 따라 추출할 수 있는 substr함수가 있습니다. 아래 코드를 실행하여 month 라는 변수를 만들고 아래 7번 문제를 해결하세요.
 #(참고: substr함수의 수행 결과는 character형태임)
@@ -82,11 +101,12 @@ install.packages("lawstat")
 library(lawstat)
 levene.test(dat$price,dat$month)
 
+#여기서부터 써도됨.
 datAnova <- aov(price ~ month, data=dat)
 summary(datAnova)
 # (5점) 7-(2). 7-(1) 결과를 해석하세요.
 
 # 해석 :
 앞서 anova검정시 p-value가 0.05보다 작다.
-이는 대립가설을 채택하는 것이며
+이는 대립가설을 채택하는 것이며(=귀무가설 기각)
 월별 평균 집값차이가 있다.
