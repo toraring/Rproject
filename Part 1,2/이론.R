f19b5ab9b3780c84b64188116154e3cf6c27f18c
bot_jang <katatj7@gmail.com>
2021-04-30 01:25:52 +0900

    173까지

--- Part 1,2/이론.R
+++ Part 1,2/이론.R
@@ -200,3 +200,40 @@ exam %>% arrange(desc(math)) # math 내림차순 정렬
 정렬 기준 변수 여러개 지정
 exam %>% arrange(class, math) # class 및 math 오름차순 정렬
 
+###파생변수
+여러 파생변수 한 번에 추가하기 
+exam %>% mutate(total = math + english + science, 
+# 총합 변수 추가
+mean = (math + english + science)/3) %>% # 총평균 변수 추가 
+  head # 일부 추출
+          
+mutate()에 ifelse() 적용하기
+exam %>%
+  mutate(test = ifelse(science >= 60, "pass", "fail")) %>%
+  head    
+추가한 변수를 dplyr 코드에 바로 활용하기
+exam %>%
+  mutate(total = math + english + science) %>% # 총합 변수 추가
+  arrange(total) %>% # 총합 변수 기준 정렬
+  head # 일부 추출
+
+#요약하기
+exam %>% summarise(mean_math = mean(math)) # math 평균 산출
+#집단요약
+exam %>%
+  group_by(class) %>% # class 별로 분리
+  summarise(mean_math = mean(math)) # math 평균 산출
+
+여러 요약통계량 한 번에 산출하기
+exam %>%
+  group_by(class) %>% # class 별로 분리
+  summarise(mean_math = mean(math), # math 평균
+            sum_math = sum(math), # math 합계
+            median_math = median(math), # math 중앙값
+            n = n()) # 학생 수
+
+각 집단별로 다시 집단 나누기
+mpg %>%
+  group_by(manufacturer, drv) %>% # 회사별, 구방방식별 분리
+  summarise(mean_cty = mean(cty)) %>% # cty 평균 산출
+  head(10) # 일부 출력
