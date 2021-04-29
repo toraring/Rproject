0499a631bc52af5290811c5fceef45d154d415ff
bot_jang <katatj7@gmail.com>
2021-04-29 21:48:50 +0900

    150p부터

--- Part 1,2/이론.R
+++ Part 1,2/이론.R
@@ -127,3 +127,76 @@ mpg$test <- ifelse(mpg$total >= 20, "pass", "fail")
 # 5.빈도 확인 
 table(mpg$test) # 빈도표 출력 
 qplot(mpg$test) # 막대 그래프 생성
+
+
+###
+#조건에 맞는 데이터만 추출하기
+#dplyr 패키지 로드 & 데이터 준비
+library(dplyr)
+exam <- read.csv("csv_exam.csv")
+exam
+# exam 에서 class 가 1 인 경우만 추출하여 출력
+exam %>% filter(class == 1)
+# 2반인 경우만 추출 
+exam %>% filter(class == 2)
+
+# 수학 점수가 50점을 초과한 경우 
+exam %>% filter(math > 50)
+# 영어점수가 80 점 이하인 경우
+exam %>% filter(english <= 80)
+
+#여러조건 충족하는 행 추출
+# 1 반 이면서 수학 점수가 50 점 이상인 경우
+exam %>% filter(class == 1 & math >= 50)
+# 2반 이면서 영어점수가 80점 이상인 경우 
+exam %>% filter(class == 2 & english >= 80)
+
+#여러 조건 중 하나 이상 충족하는 행 추출하기
+# 수학 점수가 90 점 이상이거나 영어점수가 90 점 이상인 경우
+exam %>% filter(math >= 90 | english >= 90)
+
+# 영어점수가 90점 미만이거나 과학점수가 50점 미만인 경우
+exam %>% filter(english < 90 | science < 50)
+
+목록에 해당되는 행 추출하기
+exam %>% filter(class == 1 | class == 3 | class == 5) # 1, 3, 5 반에 해당되면 추출
+%in% 기호 이용하기 
+exam %>% filter(class %in% c(1,3,5)) # 1, 3, 5 반에 해당하면 추출
+
+추출한 행으로 데이터 만들기
+class1 <- exam %>% filter(class == 1) # class 가 1 인 행 추출, class1 에 할당
+class2 <- exam %>% filter(class == 2) # class 가 2 인 행 추출, class2 에 할당
+mean(class1$math) # 1 반 수학 점수 평균 구하기
+mean(class2$math) # 2 반 수학 점수 평균 구하기
+
+2. 필요한 변수만 추출하기
+exam %>% select(math) # math 추출
+exam %>% select(english) # english 추출
+여러 변수 추출하기 
+exam %>% select(class, math, english) # class, math, english 변수 추출
+변수 제외하기 
+exam %>% select(-math) # math 제외
+exam %>% select(-math, -english) # math, english 제외
+
+dplyr 함수 조합하기 
+# class가 1인 행만 추출한 다음 english 추출 
+exam %>% filter(class == 1) %>% select(english)
+#가독성 있게 줄 바꾸기
+exam %>%
+  filter(class == 1) %>% # class 가 1 인 행 추출
+  select(english) # english 추출
+일부만 출력하기 
+exam %>% select(id, math) %>% # id, math 추출 head # 앞부분 6행까지 추출
+  
+  일부만 출력하기
+exam %>%
+  select(id, math) %>% # id, math 추출
+  head(10) # 앞부분 10 행까지 추출
+
+오름차순으로 정렬하기 
+exam %>% arrange(math) # math 오름차순 정렬
+내림차순으로 정렬하기 
+exam %>% arrange(desc(math)) # math 내림차순 정렬
+정렬 기준 변수 여러개 지정
+exam %>% arrange(class, math) # class 및 math 오름차순 정렬
+
