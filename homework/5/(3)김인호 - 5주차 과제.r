cd4681ad39ffdd48d33e14fbef59f2491a14d845
bot_jang <katatj7@gmail.com>
2021-05-19 13:52:09 +0900

    채점 및 오답정리

--- homework/5/(3)김인호 - 5주차 과제.r
+++ homework/5/(3)김인호 - 5주차 과제.r
@@ -24,7 +24,7 @@ train$Survived <- as.factor(train$Survived)
 # (5점) 2. Pclass는 Ticket 등급을 나타내는 변수입니다. 변수의 고유한 값을 확인할 수 있도록 코드를 작성해주세요. 
 # hint. unique함수 이용 
 unique(train$Pclass)
-zd
+
 
 # (5점) 3. Pclass, Age, Fare 변수들의 히스토그램을 한 눈에 볼 수 있도록 시각화 해주세요 (1x3)
 # hint. par, hist함수 이용 
@@ -33,6 +33,11 @@ hist(train$Pclass, main="train Pclass histogram", xlab="train score",col="orange
 hist(train$Age, main="train Age histogram", xlab="train score",col="green")
 hist(train$Fare, main="train Fare histogram", xlab="train score",col="blue")
 
+#좀 더 간단한 답
+par(mfrow = c(1,3))
+hist(train$Pclass)
+hist(train$Age)
+hist(train$Fare)
 
 # (5점) 4. train 전체 데이터에 대해 변수산점도를 그려주세요.
 # hint. plot함수 이용  
@@ -72,10 +77,21 @@ knn_fit <- train( Survived ~ .,
                   metric="Accuracy")
 knn_fit
 
+# 간략한 답
+customGrid <- expand.grid(k = 1:5)
+knn_fit <- train(Survived ~ .,
+                 data = train2,
+                 method = "knn",
+                 trControl = ctrl,
+                 tuneGrid = customGrid,
+                 metric = "Accuracy")
+
 
 # (5점) 8. 7번 모델에 대한 실행 결과를 해석해주세요.
 # k가 (  3  )일 때 Accuracy가 (  0.5383592  )%로 가장 높습니다. 
 
+#답
+k가 1일 때 Accuracy가 56.6%로 가장 높습니다.
 
 
 # (5점) 9. Boosted Logistic Regression 모델을 생성해주세요. (preProcess 불필요, metric = "Accuracy")
@@ -91,6 +107,8 @@ logit_boost_fit
 # (5점) 10. 9번 모델에 대한 실행 결과를 해석해주세요.
 # nIter가 (  11  )일 때 Accuracy가 (  0.7956490  )%로 가장 높습니다. 
 
+#답
+nIter가 31일 때 Accuracy가 78.5%로 가장 높습니다.
 
 
 # (5점) 11. Naive Bayes 모델을 생성해주세요. (preProcess 불필요, metric = "Accuracy")
@@ -130,3 +148,7 @@ dim(pca_data)
 reduced_data <- data.frame(cbind(pca_data[,1:3], tar))
 reduced_data$tar <- as.factor(reduced_data$tar)
 str(reduced_data)
+
+#답
+축이 4개 일 때 전체 변동성의 99%를 설명합니다.
+전체 변동성은 "Cumulative Proportion" 를 의미합니다.
