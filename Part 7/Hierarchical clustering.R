07d4a24606a881111aa7400464cf4b782edf17a5
bot_jang <katatj7@gmail.com>
2021-06-12 03:34:08 +0900

    7p까지

--- Part 7/Hierarchical clustering.R
+++ Part 7/Hierarchical clustering.R
@@ -25,3 +25,29 @@ df.rm.outlier <- df %>% rownames_to_column('rname') %>%
   slice(-1:-2) %>%
   column_to_rownames('rname')
 boxplot(df.rm.outlier)
+
+###########
+분석, 결과치 확인 및 해석
+
+1. 유사도 행렬 생성 (유클리드 거리)
+df.dist <- dist(df.rm.outlier, method = "euclidean")
+
+2. 군집 구성 방식 선택
+df.hclust.sing <- hclust(df.dist, method = "single" )
+df.hclust.cplt <- hclust(df.dist, method = "complete" )
+df.hclust.avg <- hclust(df.dist, method = "average" )
+df.hclust.cent <- hclust(df.dist, method = "centroid" )
+df.hclust.ward <- hclust(df.dist, method = "ward.D2" )
+
+3. dendrogram 생성 & 군집 시각화
+plot(df.hclust.sing, cex = 0.6, hang = -1)
+rect.hclust(df.hclust.sing, k = 4, border = 2:5)
+
+plot(df.hclust.cplt, cex = 0.6, hang = -1)
+rect.hclust(df.hclust.cplt, k = 4, border = 2:5)
+
+plot(df.hclust.avg, cex = 0.6, hang = -1)
+rect.hclust(df.hclust.avg, k = 4, border = 2:5)
+
+plot(df.hclust.cent, cex = 0.6, hang = -1)
+rect.hclust(df.hclust.cent, k = 4, border = 2:5)
