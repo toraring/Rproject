07cf0df7718c000ba200af05e09c7184be87378e
bot_jang <katatj7@gmail.com>
2021-06-16 23:44:07 +0900

    파일 내경로에 불러오기

--- Part 8/3/대학원 입시 데이터를.R
+++ Part 8/3/대학원 입시 데이터를.R
@@ -1,4 +1,4 @@
 대학원 입시 데이터를 활용한 합격률 예측
 
 ### 파일 불러오기 
-rawdata1 <- read.csv("C:/Users/Cheolwon/Documents/dataset/project/graduate/university.csv", head=TRUE)
+rawdata1 <- read.csv("C:/RProject/Rproject/Part 8/3/university.csv", head=TRUE)
