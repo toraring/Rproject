7387a19f4116b5d3c78fd0923693392626bdd82c
bot_jang <katatj7@gmail.com>
2021-06-17 22:41:11 +0900

    3차원 데이터를 2차원으로

--- Part 7/K means2.R
+++ Part 7/K means2.R
@@ -1,1 +1,14 @@
 k means2
+
+imgdim <- as.vector(dim(img))
+imgRGB <- data.frame(
+  x = rep(1:imgdim[2], each = imgdim[1]),
+  y = rep(imgdim[1]:1, imgdim[2]),
+  R = as.vector(img[,,1]),
+  G = as.vector(img[,,2]),
+  B = as.vector(img[,,3])
+)
+head(imgRGB)
+
+tail(imgRGB)
+
