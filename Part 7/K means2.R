df49d7f8308e1d1b04e95b89b9bc9742133a01ca
bot_jang <katatj7@gmail.com>
2021-06-18 00:14:04 +0900

    축소할 색상 클러스터 개수 kmeans 실습완료

--- Part 7/K means2.R
+++ Part 7/K means2.R
@@ -12,3 +12,11 @@ head(imgRGB)
 
 tail(imgRGB)
 
+kClusters <- c(3, 5, 10, 15, 30, 50) # 축소할 색상 클러스터 개수
+set.seed(1234)
+for (i in kClusters) {
+  img.kmeans <- kmeans(imgRGB[, c("R", "G", "B")], centers = i)
+  img.result <- img.kmeans$centers[img.kmeans$cluster,]
+  img.array <- array(img.result, dim = imgdim)
+  writeJPEG(img.array, paste('kmeans_', i, 'clusters.jpeg', sep = ''))
+}
