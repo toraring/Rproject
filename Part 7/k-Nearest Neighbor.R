1c47b5432a503cd006c8b18d748fc114b84e68f1
bot_jang <katatj7@gmail.com>
2021-07-08 17:47:45 +0900

    divided test

--- Part 7/k-Nearest Neighbor.R
+++ Part 7/k-Nearest Neighbor.R
@@ -7,3 +7,10 @@ awdata <- read.csv ( file= "C:/Users/파일 경로/wine.csv" , header = TRUE )
 rawdata $ Class <- as.factor (rawdata $ Class) 
 str (rawdata)
 
+#divided test data
+analdata <- rawdata
+set.seed ( 2020 )
+datatotal <- sort ( sample ( nrow (analdata), nrow (analdata) * 0.7 )) 
+train <- rawdata[datatotal,] test <- rawdata[ - datatotal,]
+str (train)
+
