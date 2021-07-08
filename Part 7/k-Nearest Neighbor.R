d70f38374a79d661f6715ada625b3edcfc0af51e
bot_jang <katatj7@gmail.com>
2021-07-08 17:48:01 +0900

    divided test 2

--- Part 7/k-Nearest Neighbor.R
+++ Part 7/k-Nearest Neighbor.R
@@ -14,3 +14,7 @@ datatotal <- sort ( sample ( nrow (analdata), nrow (analdata) * 0.7 ))
 train <- rawdata[datatotal,] test <- rawdata[ - datatotal,]
 str (train)
 
+train_x <- train[, 1 : 13 ] 
+train_y <- train[, 14 ]
+test_x <- test[, 1 : 13 ] 
+test_y <- test[, 14 ]
