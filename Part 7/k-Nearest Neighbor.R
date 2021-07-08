52019c749ca63b3770cb57b4868505fdb08eb65d
bot_jang <katatj7@gmail.com>
2021-07-08 17:47:13 +0900

    data load

--- Part 7/k-Nearest Neighbor.R
+++ Part 7/k-Nearest Neighbor.R
@@ -1,1 +1,9 @@
 #k-Nearest Neighbor
+
+#install.packages("caret", dependencies=TRUE)
+library (caret)
+#data load
+awdata <- read.csv ( file= "C:/Users/파일 경로/wine.csv" , header = TRUE )
+rawdata $ Class <- as.factor (rawdata $ Class) 
+str (rawdata)
+
