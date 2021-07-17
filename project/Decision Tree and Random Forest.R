b678644e0835f9131284f84397c90879a6e1f915
bot_jang <katatj7@gmail.com>
2021-07-18 00:08:21 +0900

    training data dviding

--- project/Decision Tree and Random Forest.R
+++ project/Decision Tree and Random Forest.R
@@ -7,3 +7,11 @@ library(caret)
 rawdata <- read.csv(file="C:/Users/Cheolwon/Documents/dataset/ml/wine.csv", header = TRUE)
 rawdata$Class <- as.factor(rawdata$Class)
 str(rawdata)
+
+#training data dviding
+analdata <- rawdata
+set.seed(2020)
+datatotal <- sort(sample(nrow(analdata), nrow(analdata)*.7))
+train <- rawdata[datatotal,]
+test <- rawdata[-datatotal,]
+str(train)
