68f9b7f2f29967fd5774f35465eae105a0d22ef9
bot_jang <katatj7@gmail.com>
2021-07-18 00:07:44 +0900

    install & data load

--- project/Decision Tree and Random Forest.R
+++ project/Decision Tree and Random Forest.R
@@ -1,1 +1,9 @@
 #[Decision Tree and Random Forest]
+
+#install.packages("caret", dependencies=TRUE)
+library(caret)
+
+#Data Load
+rawdata <- read.csv(file="C:/Users/Cheolwon/Documents/dataset/ml/wine.csv", header = TRUE)
+rawdata$Class <- as.factor(rawdata$Class)
+str(rawdata)
