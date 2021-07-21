b1b3b5aea39f815d607b57e8e0cfe954f03f0d11
bot_jang <katatj7@gmail.com>
2021-07-22 00:38:02 +0900

    install & load

--- project/Support Vector Machine.R
+++ project/Support Vector Machine.R
@@ -1,1 +1,10 @@
 #[Support Vector Machine]
+
+#install
+#install.packages("caret", dependencies=TRUE)
+library(caret)
+
+#data load
+rawdata <- read.csv(file="C:/Users/내 파일경로/wine.csv", header = TRUE)
+rawdata$Class <- as.factor(rawdata$Class)
+str(rawdata)
