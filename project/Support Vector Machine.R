3ca615e45091b739f8bca4fea5721b9516063772
bot_jang <katatj7@gmail.com>
2021-07-23 00:33:06 +0900

    선형 서포트 벡터 머신

--- project/Support Vector Machine.R
+++ project/Support Vector Machine.R
@@ -20,3 +20,13 @@ train_y <- train[,14]
 test_x <- test[,1:13]
 test_y <- test[,14]
 str(train)
+
+#선형 서포트 벡터 머신
+ctrl <- trainControl(method="repeatedcv",repeats = 5)
+svm_linear_fit <- train(Class ~ .,
+                        data = train,
+                        method = "svmLinear",
+                        trControl = ctrl,
+                        preProcess = c("center","scale"),
+                        metric="Accuracy")
+svm_linear_fit
