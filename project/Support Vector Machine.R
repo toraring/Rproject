2d8d4697f4f8ce077c8e55e8d5c303dc981cacdb
bot_jang <katatj7@gmail.com>
2021-07-23 00:32:18 +0900

    divided training test

--- project/Support Vector Machine.R
+++ project/Support Vector Machine.R
@@ -8,3 +8,15 @@ library(caret)
 rawdata <- read.csv(file="C:/Users/내 파일경로/wine.csv", header = TRUE)
 rawdata$Class <- as.factor(rawdata$Class)
 str(rawdata)
+
+#divided training test
+analdata <- rawdata
+set.seed(2020)
+datatotal <- sort(sample(nrow(analdata), nrow(analdata)*.7))
+train <- rawdata[datatotal,]
+test <- rawdata[-datatotal,]
+train_x <- train[,1:13]
+train_y <- train[,14]
+test_x <- test[,1:13]
+test_y <- test[,14]
+str(train)
