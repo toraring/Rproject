c1b2147f8c6b6d21fdd18420011841f393b88277
bot_jang <katatj7@gmail.com>
2021-07-25 00:13:30 +0900

    caret install & data load

--- project/은행 거래 데이터를 활용한 마케팅 효과 예측.R
+++ project/은행 거래 데이터를 활용한 마케팅 효과 예측.R
@@ -1,1 +1,8 @@
 #[은행 거래 데이터를 활용한 마케팅 효과 예측]
+
+#install.packages("caret", dependencies=TRUE)
+library(caret)
+
+#data load
+rawdata1 <- read.csv("C:/Users/Cheolwon/Documents/dataset/project/bankmarket/bank.csv", header=TRUE)
+str(rawdata1)
