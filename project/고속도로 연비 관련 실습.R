a98d2b962d3e5ca0dabd844fd48233631d253409
bot_jang <katatj7@gmail.com>
2021-08-03 22:20:18 +0900

    2번 문제

--- project/고속도로 연비 관련 실습.R
+++ project/고속도로 연비 관련 실습.R
@@ -2,3 +2,5 @@
 #Q1. ggplot2 패키지의 mpg 데이터를 사용할 수 있도록 불러온 뒤 복사본을 만드세요.
 mpg <- as.data.frame(ggplot2::mpg) # mpg 데이터 불러오기
 mpg_new <- mpg # 복사본 만들기
+
+#Q2. 복사본 데이터를 이용해서 cty는 city로, hwy는 highway로 변수명을 수정하세요.
