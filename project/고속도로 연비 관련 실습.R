5ff4fa6bc020dbe04af5da374e63fd9faae2841f
bot_jang <katatj7@gmail.com>
2021-08-09 02:44:04 +0900

    A2

--- project/고속도로 연비 관련 실습.R
+++ project/고속도로 연비 관련 실습.R
@@ -68,3 +68,4 @@ mpg_new <- mpg # 복사본 만들기
 mpg_new <- mpg_new %>% mutate(total = cty + hwy) # 합산 변수 만들기
 
 #Q2. 앞에서 만든 '합산 연비 변수'를 2로 나눠 '평균 연비 변수'를 추가세요.
+mpg_new <- mpg_new %>% mutate(mean = total/2) # 평균 변수 만들기
