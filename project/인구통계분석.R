8a3446c755ab58c2333dc63a1553b4aabe8ab212
bot_jang <katatj7@gmail.com>
2021-08-14 02:09:11 +0900

    A1

--- project/인구통계분석.R
+++ project/인구통계분석.R
@@ -1,2 +1,9 @@
 #[인구통계분석]
 #문제1. ggplot2의 midwest 데이터를 데이터 프레임 형태로 불러와서 데이터의 특성을 파악하세요
+midwest <- as.data.frame(ggplot2::midwest)
+head(midwest)
+tail(midwest)
+View(midwest)
+dim(midwest)
+str(midwest)
+summary(midwest)
