c05376815c57037e3fd467c6de2975c84c000ce4
bot_jang <katatj7@gmail.com>
2021-08-17 00:09:51 +0900

    연료와 가격의 관계 시작 (프레임 형성)

---
+++ project/연료와 가격의 관계.R
@@ -0,0 +1,6 @@
+#[연료와 가격의 관계]
+#데이터프레임
+fuel <- data.frame(fl = c("c", "d", "e", "p", "r"),
+                   price_fl = c(2.35, 2.38, 2.11, 2.76, 2.22),
+                   stringsAsFactors = F)
+fuel # 출력
