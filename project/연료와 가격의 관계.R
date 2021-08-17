708fc6b850462ac9553e9189b8912cef1ae98e3e
bot_jang <katatj7@gmail.com>
2021-08-18 00:52:25 +0900

    Q1

--- project/연료와 가격의 관계.R
+++ project/연료와 가격의 관계.R
@@ -4,3 +4,4 @@ fuel <- data.frame(fl = c("c", "d", "e", "p", "r"),
                    price_fl = c(2.35, 2.38, 2.11, 2.76, 2.22),
                    stringsAsFactors = F)
 fuel # 출력
+#Q1. mpg 데이터에는 연료 종류를 나타낸 fl 변수는 있지만 연료 가격을 나타낸 변수는 없습니다. 위에서 만든 fuel 데이터를 이용해서 mpg 데이터에 price_fl(연료 가격) 변수를 추가하세요.
