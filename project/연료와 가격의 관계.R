7c02049e1f9995b3a22f6478cb4154a955ff06d3
bot_jang <katatj7@gmail.com>
2021-08-18 00:53:15 +0900

    A2

--- project/연료와 가격의 관계.R
+++ project/연료와 가격의 관계.R
@@ -9,3 +9,6 @@ mpg <- as.data.frame(ggplot2::mpg) # mpg 데이터 불러오기
 mpg <- left_join(mpg, fuel, by = "fl") # mpg 에 연료 가격 변수 추가
 
 #Q2. 연료 가격 변수가 잘 추가됐는지 확인하기 위해서 model, fl, price_fl 변수를 추출해 앞부분 5행을 출력해 보세요.
+mpg %>%
+  select(model, fl, price_fl) %>% # model, fl, price_fl 추출
+  head(5)
