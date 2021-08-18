3865c549df55fbe5acd3d3c0ddf7879fceb7c27b
bot_jang <katatj7@gmail.com>
2021-08-19 01:17:32 +0900

    A1

--- project/미국 인구 통계.R
+++ project/미국 인구 통계.R
@@ -1,2 +1,8 @@
 #[미국 인구 통계]
 #문제1. popadults는 해당 지역의 성인 인구, poptotal은 전체 인구를 나타냅니다. midwest 데이터에 '전체 인구 대비 미성년 인구 백분율' 변수를 추가하세요.
+# midwest 불러오기
+midwest <- as.data.frame(ggplot2::midwest)
+
+# midwest 에 백분율 변수 추가
+midwest <- midwest %>%
+  mutate(ratio_child = (poptotal-popadults)/poptotal*100)
