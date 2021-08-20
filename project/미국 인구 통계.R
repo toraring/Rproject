e5fd46d76f5b2c005f9c3e5ab8678a87e430c669
bot_jang <katatj7@gmail.com>
2021-08-21 01:21:48 +0900

    Q4

--- project/미국 인구 통계.R
+++ project/미국 인구 통계.R
@@ -23,3 +23,5 @@ midwest <- midwest %>%
 table(midwest$grade)
 ## large middle small
 ## 32 396 9
+
+#문제4. popasian은 해당 지역의 아시아인 인구를 나타냅니다. '전체 인구 대비 아시아인 인구 백분율' 변수를 추가하고, 하위 10개 지역의 state(주), county(지역명), 아시아인 인구 백분율을 출력하세요.
