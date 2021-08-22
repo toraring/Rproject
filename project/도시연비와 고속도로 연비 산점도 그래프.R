06a12a94ed83bca0ab99c8c65e3ab5363ab0bf48
bot_jang <katatj7@gmail.com>
2021-08-22 12:59:32 +0900

    A2

--- project/도시연비와 고속도로 연비 산점도 그래프.R
+++ project/도시연비와 고속도로 연비 산점도 그래프.R
@@ -5,6 +5,10 @@
 ggplot(data = mpg, aes(x = cty, y = hwy)) + geom_point()
 
 #• Q2. 미국 지역별 인구통계 정보를 담은 ggplot2 패키지의 midwest 데이터를 이용해서 전체 인구와 아시아인 인구 간에 어떤 관계가 있는지 알아보려고 합니다. x 축은 poptotal(전체 인구), y 축은 popasian(아시아인 인구)으로 된 산점도를 만들어 보세요. 전체 인구는 50 만 명 이하, 아시아인 인구는 1 만 명 이하인 지역만 산점도에 표시되게 설정하세요.
+ggplot(data = midwest, aes(x = poptotal, y = popasian)) +
+  geom_point() +
+  xlim(0, 500000) +
+  ylim(0, 10000)
 
 #HINT
 #Q1. geom_point()를 이용해 산점도를 만들어 보세요.
