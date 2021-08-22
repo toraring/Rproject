4edc5a238de97115e80d9880530f6bf73285f9a3
bot_jang <katatj7@gmail.com>
2021-08-23 01:03:51 +0900

    A1

--- project/mpg데이터를 이용하여 도시 연비 구하기.R
+++ project/mpg데이터를 이용하여 도시 연비 구하기.R
@@ -1,2 +1,13 @@
 ###mpg데이터를 이용하여 도시 연비 구하기
 #Q1. 어떤 회사에서 생산한 "suv" 차종의 도시 연비가 높은지 알아보려고 합니다. "suv" 차종을 대상으로 평균 cty(도시 연비)가 가장 높은 회사 다섯 곳을 막대 그래프로 표현해 보세요. 막대는 연비 가 높은 순으로 정렬하세요.
+# 평균 표 생성
+df <- mpg %>%
+  filter(class == "suv") %>%
+  group_by(manufacturer) %>%
+  summarise(mean_cty = mean(cty)) %>%
+  arrange(desc(mean_cty)) %>%
+  head(5)
+
+# 그래프 생성
+ggplot(data = df, aes(x = reorder(manufacturer, -mean_cty),
+                      y = mean_cty)) + geom_col()
