5804377e4938f1c9554a7c1f74518b76863d58ae
bot_jang <katatj7@gmail.com>
2021-08-26 22:55:44 +0900

    A1

--- project/자동차 종류에 따른 도시연비 비교.R
+++ project/자동차 종류에 따른 도시연비 비교.R
@@ -1,2 +1,6 @@
 ##[자동차 종류에 따른 도시연비 비교]
 #Q1. class(자동차 종류)가 "compact", "subcompact", "suv"인 자동차의 cty(도시 연비)가 어떻게 다른지 비교해보려고 합니다. 세 차종의 cty를 나타낸 상자 그림을 만들어보세요.
+class_mpg <- mpg %>%
+  filter(class %in% c("compact", "subcompact", "suv"))
+
+ggplot(data = class_mpg, aes(x = class, y = cty)) + geom_boxplot()
