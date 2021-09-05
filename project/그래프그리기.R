fe149f8d4b6de54963b67e398d1219cd92392c77
bot_jang <katatj7@gmail.com>
2021-09-05 14:09:44 +0900

    크기순으로 정렬

--- project/그래프그리기.R
+++ project/그래프그리기.R
@@ -55,3 +55,5 @@ df_mpg
 
 #2. 그래프 생성하기
 ggplot(data = df_mpg, aes(x = drv, y = mean_hwy)) + geom_col()
+#크기 순으로 정렬하기
+ggplot(data = df_mpg, aes(x = reorder(drv, -mean_hwy), y = mean_hwy)) + geom_col()
