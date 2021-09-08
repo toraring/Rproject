3484a92fdeac280c0a9ccc84bb8aae88f877aa19
bot_jang <katatj7@gmail.com>
2021-09-08 13:20:02 +0900

    정리

--- project/그래프그리기.R
+++ project/그래프그리기.R
@@ -83,3 +83,32 @@ ggplot(data = economics, aes(x = date, y = unemploy)) + geom_line()
 #- 분포를 알 수 있기 때문에 평균만 볼 때보다 데이터의 특성을 좀 더 자세히 이해할 수 있음
 #상자그림만들기
 ggplot(data = mpg, aes(x = drv, y = hwy)) + geom_boxplot()
+
+#[정리]
+# 1.산점도
+ggplot(data = mpg, aes(x = displ, y = hwy)) + geom_point()
+
+# 축 설정 추가
+ggplot(data = mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  xlim(3, 6) +
+  ylim(10, 30)
+
+# 2.평균 막대 그래프
+
+# 1 단계.평균표 만들기
+df_mpg <- mpg %>%
+  group_by(drv) %>%
+  summarise(mean_hwy = mean(hwy))
+
+# 2 단계.그래프 생성하기, 크기순 정렬하기
+ggplot(data = df_mpg, aes(x = reorder(drv, -mean_hwy), y = mean_hwy)) + geom_col()
+
+# 3.빈도 막대 그래프
+ggplot(data = mpg, aes(x = drv)) + geom_bar()
+
+# 4.선 그래프
+ggplot(data = economics, aes(x = date, y = unemploy)) + geom_line()
+
+# 5.상자 그림
+ggplot(data = mpg, aes(x = drv, y = hwy)) + geom_boxplot()
