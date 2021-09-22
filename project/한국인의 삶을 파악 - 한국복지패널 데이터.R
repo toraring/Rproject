eb8e9ae5a9ad8629a8a510c8416ea711f018f781
bot_jang <katatj7@gmail.com>
2021-09-22 15:14:12 +0900

    이상치 확인 및 결측처리

--- project/한국인의 삶을 파악 - 한국복지패널 데이터.R
+++ project/한국인의 삶을 파악 - 한국복지패널 데이터.R
@@ -40,3 +40,14 @@ class(welfare$sex)
 summary(welfare$sex)
 
 table(welfare$sex)
+
+#1-2.정제 - 이상치 확인 및 결측처리
+#• 성별 이상치 : 모름/무응답=9
+# 이상치 확인
+table(welfare$sex)
+
+# 이상치 결측 처리
+welfare$sex <- ifelse(welfare$sex == 9, NA, welfare$sex)
+
+# 결측치 확인
+table(is.na(welfare$sex))
