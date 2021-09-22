cb1443c0a146d7af407143d499163b3aa7b53ac8
bot_jang <katatj7@gmail.com>
2021-09-22 15:14:36 +0900

    변수값 변경

--- project/한국인의 삶을 파악 - 한국복지패널 데이터.R
+++ project/한국인의 삶을 파악 - 한국복지패널 데이터.R
@@ -51,3 +51,10 @@ welfare$sex <- ifelse(welfare$sex == 9, NA, welfare$sex)
 
 # 결측치 확인
 table(is.na(welfare$sex))
+
+##변수 값 변경
+# 항목 이름 부여
+welfare$sex <- ifelse(welfare$sex == 1, "male", "female")
+table(welfare$sex)
+
+qplot(welfare$sex)
