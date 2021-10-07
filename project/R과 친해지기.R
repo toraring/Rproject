18caa888983b2e40b34af1fa447c76a18c0033ab
bot_jang <katatj7@gmail.com>
2021-10-07 21:57:59 +0900

    데이터 복구

--- project/R과 친해지기.R
+++ project/R과 친해지기.R
@@ -1,17 +1,11 @@
-#R과 친해지기
-#패키지 설치 
+패키지 설치 
 install.packages("dplyr") install.packages("ggplot2")
 
-#패키지 로드 
+패키지 로드 
 library(dplyr) library(ggplot2)
 
-#데이터 검토
+데이터 검토
 head(mpg)
 dim(mpg)
 str(mpg)
 summary(mpg)
-
-#• 1.회사별 평균 연비 높은순 정렬
-mpg %>% 
-  group_by(manufacturer) %>%
-  summarise(mean.hwy=mean(hwy))
