b3afbc4af9de6de45db9f60787adf967abae177e
bot_jang <katatj7@gmail.com>
2021-10-21 02:25:05 +0900

    Arragne data

--- project/데이터 파악하기, 다루기 쉽게 수정하기.R
+++ project/데이터 파악하기, 다루기 쉽게 수정하기.R
@@ -1,1 +1,7 @@
 #데이터 파악하기, 다루기 쉽게 수정하기
+#데이터 준비
+exam <- read.csv("csv_exam.csv")
+head(exam) # 앞에서부터 6 행까지 출력
+head(exam, 10) # 앞에서부터 10 행까지 출력
+tail(exam) # 뒤에서부터 6 행까지 출력
+tail(exam, 10) # 뒤에서부터 10 행까지 출력
