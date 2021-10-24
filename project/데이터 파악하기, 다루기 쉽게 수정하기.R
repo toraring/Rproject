2f888e21254bb636ef81b4e5611f084974b07d65
bot_jang <katatj7@gmail.com>
2021-10-25 01:48:53 +0900

    additional checking

--- project/데이터 파악하기, 다루기 쉽게 수정하기.R
+++ project/데이터 파악하기, 다루기 쉽게 수정하기.R
@@ -21,3 +21,10 @@ summary(exam) # 요약통계량 출력
 mpg <- as.data.frame(ggplot2::mpg)
 head(mpg) # Raw 데이터 앞부분 확인
 tail(mpg) # Raw 데이터 뒷부분 확인
+
+View(mpg) # Raw 데이터 뷰어 창 확인
+dim(mpg) # 행, 열 출력
+## [1] 234 11
+
+str(mpg) # 데이터 속성 확인
+summary(mpg) # 요약통계량 출력
