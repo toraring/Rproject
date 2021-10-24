cad23b653e3cebaa639c215289c4f0bb4ca8e224
bot_jang <katatj7@gmail.com>
2021-10-24 18:11:16 +0900

    ggplo2 의 mpg 데이터를 데이터 프레임 형태로 불러오기

--- project/데이터 파악하기, 다루기 쉽게 수정하기.R
+++ project/데이터 파악하기, 다루기 쉽게 수정하기.R
@@ -16,3 +16,8 @@ dim(exam) # 행, 열 출력
 
 str(exam) # 데이터 속성 확인
 summary(exam) # 요약통계량 출력
+
+## ggplo2 의 mpg 데이터를 데이터 프레임 형태로 불러오기
+mpg <- as.data.frame(ggplot2::mpg)
+head(mpg) # Raw 데이터 앞부분 확인
+tail(mpg) # Raw 데이터 뒷부분 확인
