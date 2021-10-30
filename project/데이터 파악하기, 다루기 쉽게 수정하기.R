43950f31093b6061f70982c6ed4b8a68af3bb40c
bot_jang <katatj7@gmail.com>
2021-10-30 20:17:42 +0900

    파생변수 생성

--- project/데이터 파악하기, 다루기 쉽게 수정하기.R
+++ project/데이터 파악하기, 다루기 쉽게 수정하기.R
@@ -53,3 +53,10 @@ df <- data.frame(var1 = c(4, 3, 8),
                  var2 = c(2, 6, 1)) 
 
 df
+
+#파생변수 생성 
+df$var_sum <- df$var1 + df$var2 # var_sum 파생변수 생성 
+df
+
+df$var_mean <- (df$var1 + df$var2)/2 # var_mean 파생변수 생성
+df
