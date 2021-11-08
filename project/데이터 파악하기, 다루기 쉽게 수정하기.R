78d7204608fd203f8cfee7e5b159cc18e1d32620
bot_jang <katatj7@gmail.com>
2021-11-08 14:08:22 +0900

    중첩조건문 활용

--- project/데이터 파악하기, 다루기 쉽게 수정하기.R
+++ project/데이터 파악하기, 다루기 쉽게 수정하기.R
@@ -113,3 +113,9 @@ head(mpg, 20) # 데이터 확인
 #4. 막대 빈도 표현하기
 library(ggplot2) # ggplot2 로드
 qplot(mpg$test) # 연비 합격 빈도 막대 그래프 생성
+
+#중첩조건문 활용
+#NP # total을 기준으로 A, B, C 등급 부여 
+mpg$grade <- ifelse(mpg$total >= 30, "A",
+                    ifelse(mpg$total >= 20, "B", "C")) 
+head(mpg, 20) # 데이터 확인
