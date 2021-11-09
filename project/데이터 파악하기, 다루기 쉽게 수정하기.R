4ca658f83a6fb4cedf9e594968a2eb640133d08c
bot_jang <katatj7@gmail.com>
2021-11-09 22:52:30 +0900

    범위 부여

--- project/데이터 파악하기, 다루기 쉽게 수정하기.R
+++ project/데이터 파악하기, 다루기 쉽게 수정하기.R
@@ -126,3 +126,8 @@ table(mpg$grade) # 등급 빈도표 생성
 ## 10 118 106
 
 qplot(mpg$grade) # 등급 빈도 막대 그래프 생성
+
+# A, B, C, D 등급 부여 
+mpg$grade2 <- ifelse(mpg$total >= 30, "A",
+                     ifelse(mpg$total >= 25, "B",
+                            ifelse(mpg$total >= 20, "C", "D")))
