cbab9039302883c1679921f230d2dbb883da8ad2
bot_jang <katatj7@gmail.com>
2021-11-13 12:12:50 +0900

    빈도확인

--- project/데이터 파악하기, 다루기 쉽게 수정하기.R
+++ project/데이터 파악하기, 다루기 쉽게 수정하기.R
@@ -154,3 +154,6 @@ mpg <- rename(mpg, company = manufacturer)
 mpg$total <- (mpg$cty + mpg$hwy)/2 # 변수 조합 
 mpg$test <- ifelse(mpg$total >= 20, "pass", "fail") # 조건문 활용
 
+# 5.빈도 확인 
+table(mpg$test) # 빈도표 출력
+qplot(mpg$test) # 막대 그래프 생성
