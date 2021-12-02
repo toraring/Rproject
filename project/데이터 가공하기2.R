f9068f083275d094dbada164bc80f2ac4da9a431
bot_jang <katatj7@gmail.com>
2021-12-02 23:35:39 +0900

    파생변수 추가

--- project/데이터 가공하기2.R
+++ project/데이터 가공하기2.R
@@ -4,3 +4,8 @@ exam %>% arrange(desc(math)) # math 내림차순 정렬
 
 #정렬 기준 변수 여러개 지정
 exam %>% arrange(class, math) # class 및 math 오름차순 정렬
+
+#파생변수
+exam %>% 
+  mutate(total = math + english + science) %>% # 총합 변수 추가 
+  head # 일부 추출
