efcc9d48350530ed6b51c81336ba5fcad384b13b
bot_jang <katatj7@gmail.com>
2021-12-16 00:09:42 +0900

    generated the data

--- project/데이터 가공하기2.R
+++ project/데이터 가공하기2.R
@@ -81,3 +81,12 @@ name
 #class 기준 합치기
 exam_new <- left_join(exam, name, by = "class")
 exam_new
+
+#데이터 생성
+# 학생 1~5 번 시험 데이터 생성
+group_a <- data.frame(id = c(1, 2, 3, 4, 5),
+                      test = c(60, 80, 70, 90, 85))
+
+# 학생 6~10 번 시험 데이터 생성
+group_b <- data.frame(id = c(6, 7, 8, 9, 10),
+                      test = c(70, 83, 65, 95, 80))
