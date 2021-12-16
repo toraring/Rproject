8cc7889b73de889ef844fcb24769c1932ec697ea
bot_jang <katatj7@gmail.com>
2021-12-17 02:22:26 +0900

    그룹 출력

--- project/데이터 가공하기2.R
+++ project/데이터 가공하기2.R
@@ -90,3 +90,6 @@ group_a <- data.frame(id = c(1, 2, 3, 4, 5),
 # 학생 6~10 번 시험 데이터 생성
 group_b <- data.frame(id = c(6, 7, 8, 9, 10),
                       test = c(70, 83, 65, 95, 80))
+
+group_a # group_a 출력
+group_b # group_b 출력
